fe206c780c033836418d5e27a60e3663ab958947
SteffenKraft <steffen.kraft@mail.de>
2022-06-03 14:48:48 +0200

    add async count extraReducer with createAsyncThunk

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -6,28 +6,40 @@ import {
   reset,
   increment,
   incrementByAmount,
-  selectCount,
+  incrementAsync,
+  selectCountStatus,
+  selectCountValue,
 } from "./counterSlice";
 
 export function Counter() {
-  const count = useAppSelector(selectCount);
+  const counterValue = useAppSelector(selectCountValue);
+  const counterStatus = useAppSelector(selectCountStatus);
   const dispatch = useAppDispatch();
   const [incrementAmount, setIncrementAmount] = useState("2");
   const incrementValue = Number(incrementAmount) || 0;
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIncrementAmount(e.target.value);
   };
+
   return (
     <div>
       <div>
         <button onClick={() => dispatch(decrement())}>-</button>
-        <span>{count}</span>
+        <span>{counterValue}</span>
         <button onClick={() => dispatch(increment())}>+</button>
       </div>
       <div>
         <input value={incrementAmount} onChange={handleAmountChange} />
         <button onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
+        </button>
+      </div>
+      <div>
+        <button
+          disabled={counterStatus === "loading"}
+          onClick={() => dispatch(incrementAsync(incrementValue))}
+        >
+          Add Async Amount
         </button>
       </div>
       <button onClick={() => dispatch(reset())}>reset</button>
