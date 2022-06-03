8e064e2024dfd88ce064ae028466546ceb586689
SteffenKraft <steffen.kraft@mail.de>
2022-06-03 11:42:13 +0200

    adding styled-components

---
+++ src/components/header/Header.styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  height: 100px;
+  background-color: #f3f3f3;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
+import * as Styled from "./Header.styled";
 
 const Header = (): JSX.Element => (
-  <>
+  <Styled.Header>
     <h1>Header</h1>
     <Link to="/">Home</Link>
     <Link to="/posts">Posts</Link>
     <Link to="/todos">Todos</Link>
-  </>
+  </Styled.Header>
 );
 
 export default Header;
